5f83abef8e33f4930e917813baabc0bc2c5d97de
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-11-23 14:40:55 +0100

    boot: device_tree: Mark all modules as pub

--- src/boot/device_tree/mod.rs
+++ src/boot/device_tree/mod.rs
@@ -1,9 +1,9 @@
 //! Abstraction of the Device Tree.
 
 pub mod dt;
-mod header;
-mod memory_reservation_block;
-mod node;
-mod parser;
-mod property;
-mod structure_block;
+pub mod header;
+pub mod memory_reservation_block;
+pub mod node;
+pub mod parser;
+pub mod property;
+pub mod structure_block;
