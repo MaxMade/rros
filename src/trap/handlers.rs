5667740e135490222f1de9f69432ce7306158867
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-11-23 14:40:56 +0100

    trap: Implement interface for registering trap handlers

--- src/trap/handlers.rs
+++ src/trap/handlers.rs
@@ -1,22 +1,114 @@
 //! Software-Abstractions for trap handlers.
 
+use core::array;
 use core::mem;
+use core::sync::atomic::AtomicBool;
+use core::sync::atomic::Ordering;
 
+use crate::drivers::panic::PANIC;
 use crate::kernel::cpu::STVec;
 use crate::kernel::cpu::STVecMode;
+use crate::sync::init_cell::InitCell;
 use crate::sync::level::LevelEpilogue;
 use crate::sync::level::LevelInitialization;
 use crate::sync::level::LevelPrologue;
+use crate::sync::per_core::PerCore;
 use crate::trap::cause::Trap;
+
+const NUM_EXCEPTION_HANDLERS: usize = 256;
+const NUM_INTERRUPT_HANDLERS: usize = 256;
+
+/// Instance for registering/requesting [`TrapHandler`]s.
+pub static TRAP_HANDLERS: InitCell<TrapHandlers> = InitCell::new();
+
+/// Abstraction of trap handlers
+pub struct TrapHandlers {
+    exception_handlers: [&'static dyn TrapHandler; NUM_EXCEPTION_HANDLERS],
+    interrupt_handlers: [&'static dyn TrapHandler; NUM_INTERRUPT_HANDLERS],
+    globally_pending: AtomicBool,
+    locally_pending_interrupt: PerCore<[AtomicBool; NUM_INTERRUPT_HANDLERS]>,
+    locally_pending_exception: PerCore<[AtomicBool; NUM_EXCEPTION_HANDLERS]>,
+}
+
+impl TrapHandlers {
+    /// Prepare [`TRAP_HANDLERS`].
+    pub fn initailize(token: LevelInitialization) -> LevelInitialization {
+        // Get mutable reference for TRAP_HANDLERS
+        let (handlers, token) = TRAP_HANDLERS.as_mut(token);
+
+        // Initialize members
+        let panic: &'static dyn TrapHandler = &PANIC;
+        handlers.exception_handlers = [panic; NUM_EXCEPTION_HANDLERS];
+        handlers.interrupt_handlers = [panic; NUM_INTERRUPT_HANDLERS];
+
+        handlers.globally_pending.store(false, Ordering::Relaxed);
+
+        handlers.locally_pending_interrupt =
+            PerCore::new_fn(|_| array::from_fn(|_| AtomicBool::new(false)));
+        handlers.locally_pending_exception =
+            PerCore::new_fn(|_| array::from_fn(|_| AtomicBool::new(false)));
+
+        token
+    }
+
+    /// Register `handler` for `trap`
+    ///
+    /// # Panic
+    /// If another `handler` is already register for `trap`, this function will panic!
+    pub fn register(
+        &self,
+        trap: Trap,
+        handler: &'static dyn TrapHandler,
+        token: LevelInitialization,
+    ) -> LevelInitialization {
+        // Get mutable reference for TRAP_HANDLERS
+        let (handlers, token) = TRAP_HANDLERS.as_mut(token);
+
+        let panic: &'static dyn TrapHandler = &PANIC;
+        match trap {
+            Trap::Interrupt(interrupt) => {
+                let index: usize = interrupt.into();
+                if handlers.interrupt_handlers[index] as *const _ == panic as *const _ {
+                    panic!(
+                        "Unable to overwrite handler for {} at trap handlers interface",
+                        interrupt
+                    );
+                }
+                handlers.interrupt_handlers[index] = handler;
+            }
+            Trap::Exception(exception) => {
+                let index: usize = exception.into();
+                if handlers.exception_handlers[index] as *const _ == panic as *const _ {
+                    panic!(
+                        "Unable to overwrite handler for {} at trap handlers interface",
+                        exception
+                    );
+                }
+                handlers.exception_handlers[index] = handler;
+            }
+        }
+
+        token
+    }
+
+    /// Finish initialization of [`TRAP_HANDLERS`] after all drivers registered their corresponding
+    /// handlers.
+    pub fn finalize(token: LevelInitialization) -> LevelInitialization {
+        let token = unsafe { TRAP_HANDLERS.finanlize(token) };
+        token
+    }
+}
 
 extern "C" {
     fn __trap_entry();
 }
 
 /// Interface for handling traps -  suitable for interrupts and exceptions.
-pub trait TrapHandler {
+pub trait TrapHandler: Sync {
     /// Get [`Trap`] cause.
-    fn cause() -> Trap;
+    fn cause() -> Trap
+    where
+        Self: Sized;
 
     /// High-priority task of Prologue/Epilogue model.
     ///
